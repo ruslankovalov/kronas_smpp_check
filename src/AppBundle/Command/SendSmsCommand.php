fd35283711d7ab16db7037e8221193c636093fbe
ruslan <ruslan@ekreative.com>
2017-09-11 09:14:00 +0000

    Change SmppClientBundle to forked lib

--- src/AppBundle/Command/SendSmsCommand.php
+++ src/AppBundle/Command/SendSmsCommand.php
@@ -9,10 +9,7 @@
 namespace AppBundle\Command;
 
 
-use Kronas\SmppClientBundle\Encoder\GsmEncoder;
-use Kronas\SmppClientBundle\Service\SmppTransmitter;
-use Kronas\SmppClientBundle\SMPP;
-use Kronas\SmppClientBundle\SmppCore\SmppAddress;
+use Kronas\SmppClientBundle\Transmitter\SmppTransmitter;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
@@ -31,7 +28,7 @@ class SendSmsCommand extends Command
     public function __construct(Container $container)
     {
         $this->container = $container;
-        parent::__construct('smpp:send:sms');
+        parent::__construct('smpp:sms:send');
     }
 
     protected function configure()
@@ -40,6 +37,8 @@ class SendSmsCommand extends Command
         $this->addArgument('message', InputArgument::OPTIONAL, 'The message itself', 'test message');
         $this->addArgument('operator', InputArgument::OPTIONAL, 'kyivstar or vip', 'kyivstar');
         $this->addArgument('sender', InputArgument::OPTIONAL, 'This is sender name', 'testsms');
+        $this->addArgument('encoding', InputArgument::OPTIONAL, 'encoding "cyr" or "gsm"', 'gsm');
+        $this->addArgument('data_coding', InputArgument::OPTIONAL, 'data_coding', 0);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -48,27 +47,16 @@ class SendSmsCommand extends Command
         $operator = $input->getArgument('operator');
         $message = $input->getArgument('message');
         $sender = $input->getArgument('sender');
+        $encoding = $input->getArgument('encoding');
+        $dataCoding = $input->getArgument('data_coding');
         if ($operator == 'kyivstar') {
-            $smpp = new SmppTransmitter([['5.178.83.10'], 2778, 1000], 'kidslox', 'GZpnO5H4', 'testsms', ['transport' => true, 'smpp' => true]);
+            $smpp = new SmppTransmitter([['5.178.83.10'], 2778, 1000], 'kidslox', 'GZpnO5H4', $sender, '', ['transport' => true, 'smpp' => true]);
         } elseif ($operator == 'vip') {
-            $smpp = new SmppTransmitter([['77.243.16.54'], 2775, 1000], 'kids-lox', '6wbr-4', 'testsms', ['transport' => true, 'smpp' => true]);
+            $smpp = new SmppTransmitter([['77.243.16.54'], 2775, 1000], 'kids-lox', '6wbr-4', $sender, '',['transport' => true, 'smpp' => true]);
         } else {
             throw new InvalidArgumentException('There is no place for "else". Operators should be very specific.');
         }
 
-        //TODO Fork EitherSoft/SmppClientBundle (kronas/smpp-client-bundle) and make openSmppConnection closeSmppConnection public and add getSmpp
-//        $message = 'КИЇВСТАР Батьківській контроль. Код доступу 000000';
-
-        $message = GsmEncoder::utf8_to_gsm0338($message);
-//        $message = mb_convert_encoding($message, 'ISO-8859-5', 'UTF-8');
-//        $message = mb_convert_encoding($message, 'UTF-8', 'ISO-8859-5');
-        $from = new SmppAddress($sender, SMPP::TON_ALPHANUMERIC);
-        $to = new SmppAddress(intval($phone), SMPP::TON_INTERNATIONAL, SMPP::NPI_E164);
-
-        $smpp->openSmppConnection();
-        $messageId = $smpp->getSmpp()->sendSMS($from, $to, $message, null, 0);
-        $output->writeln('Message ID: '.$messageId);
-        $smpp->closeSmppConnection();
-//        $smpp->send('380936538334', 'КИЇВСТАР Батьківській контроль. Код доступу 000000');
+        $smpp->send($phone, $message, $encoding, $dataCoding);
     }
 }
